c8e7f83e9731b427b9b2852d6a2280de6fdc76b0
suntao <stazxr@qq.com>
2022-02-07 21:41:55 +0800

    集成redis

---
+++ zblog-core/src/main/java/com/github/stazxr/zblog/core/config/RedisConfig.java
@@ -0,0 +1,65 @@
+package com.github.stazxr.zblog.core.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * Redis配置
+ *
+ * @author SunTao
+ * @since 2022-02-07
+ */
+@ConditionalOnClass(ObjectMapper.class)
+@Configuration(proxyBeanMethods = false)
+public class RedisConfig {
+    /**
+     * Redis template redis template.
+     *
+     * @param redisConnectionFactory the redis connection factory
+     * @return the redis template
+     */
+    @Bean("redisTemplate")
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = initJacksonSerializer();
+
+        // 设置value的序列化规则和 key的序列化规则
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    /**
+     * 处理redis序列化问题
+     * @return Jackson2JsonRedisSerializer
+     */
+    private Jackson2JsonRedisSerializer<Object> initJacksonSerializer() {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        // om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(om.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+
+        // Jackson2反序列化数据处理LocalDateTime类型时出错
+        om.disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+
+        // java8 时间支持
+        om.registerModule(new JavaTimeModule());
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        return jackson2JsonRedisSerializer;
+    }
+}
