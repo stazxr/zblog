07d7411d9f17d8c3e8715369a28ae4464196a3f5
SunTao <1027353579@qq.com>
2023-02-27 03:43:57 +0800

    访客记录接口新增记录每日访客数的功能

--- zblog-service/src/main/java/com/github/stazxr/zblog/mapper/VisitorMapper.java
+++ zblog-service/src/main/java/com/github/stazxr/zblog/mapper/VisitorMapper.java
@@ -2,6 +2,7 @@ package com.github.stazxr.zblog.mapper;
 
 import com.github.stazxr.zblog.core.base.BaseMapper;
 import com.github.stazxr.zblog.domain.entity.Visitor;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * 访客数据持久层
@@ -14,4 +15,27 @@ public interface VisitorMapper extends BaseMapper<Visitor> {
      * 增加网站访问量
      */
     void addWebVisitorCount();
+
+    /**
+     * 日期数据是否存在
+     *
+     * @param date 日期
+     * @return boolean
+     */
+    boolean existsDate(@Param("date") String date);
+
+    /**
+     * 新增日期访问量
+     *
+     * @param id   ID
+     * @param date 日期
+     */
+    void insertWebVisitorTodayCount(@Param("date") String date);
+
+    /**
+     * 更新日期访问量
+     *
+     * @param date 日期
+     */
+    void addWebVisitorTodayCount(@Param("date") String date);
 }
