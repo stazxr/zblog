3065ebf9b5b01e25f3e50c79e41d8b75768b0e5c
SunTao <1027353579@qq.com>
2023-03-21 09:47:26 +0800

    邮箱管理接口文档补齐

--- zblog-base/src/main/java/com/github/stazxr/zblog/base/controller/EmailController.java
+++ zblog-base/src/main/java/com/github/stazxr/zblog/base/controller/EmailController.java
@@ -3,7 +3,7 @@ package com.github.stazxr.zblog.base.controller;
 import com.github.stazxr.zblog.base.component.email.MailReceiveHandler;
 import com.github.stazxr.zblog.base.component.email.MailService;
 import com.github.stazxr.zblog.base.util.Constants;
-import com.github.stazxr.zblog.core.annotation.RequestPostSingleParam;
+import com.github.stazxr.zblog.core.annotation.ApiVersion;
 import com.github.stazxr.zblog.core.annotation.Router;
 import com.github.stazxr.zblog.core.base.BaseConst;
 import com.github.stazxr.zblog.core.model.Result;
@@ -14,6 +14,8 @@ import com.github.stazxr.zblog.util.StringUtils;
 import com.github.stazxr.zblog.util.UuidUtils;
 import com.github.stazxr.zblog.util.math.RandomUtils;
 import com.github.stazxr.zblog.util.time.DateUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -30,6 +32,7 @@ import org.thymeleaf.context.Context;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/email")
+@Api(value = "EmailController", tags = { "邮箱控制器" })
 public class EmailController {
     private final MailService mailService;
 
@@ -42,8 +45,10 @@ public class EmailController {
      * @return 验证码缓存key
      */
     @PostMapping("sendCode")
+    @ApiOperation(value = "发送邮箱验证码")
+    @ApiVersion(group = { BaseConst.ApiVersion.V_4_0_0 })
     @Router(name = "发送邮箱验证码", code = "sendCode", level = BaseConst.PermLevel.OPEN)
-    public Result sendCode(@RequestPostSingleParam String email) {
+    public Result sendCode(@RequestParam String email) {
         Assert.isTrue(StringUtils.isBlank(email), "邮箱不能为空");
         Assert.isTrue(!RegexUtils.match(email, RegexUtils.Const.EMAIL_REGEX), "邮箱格式不正确");
 
