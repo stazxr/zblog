56e14781c708f8207abb896c9c80c1f84090ce5b
SunTao <1027353579@qq.com>
2023-03-21 09:10:05 +0800

    健康检查接口文档补齐

--- zblog-base/src/main/java/com/github/stazxr/zblog/base/controller/HealthCheckController.java
+++ zblog-base/src/main/java/com/github/stazxr/zblog/base/controller/HealthCheckController.java
@@ -1,8 +1,11 @@
 package com.github.stazxr.zblog.base.controller;
 
+import com.github.stazxr.zblog.core.annotation.ApiVersion;
 import com.github.stazxr.zblog.core.annotation.Router;
 import com.github.stazxr.zblog.core.base.BaseConst;
 import com.github.stazxr.zblog.core.model.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-10-19
  */
 @RestController
+@Api(value = "HealthCheckController", tags = { "健康检查控制器" })
 public class HealthCheckController {
     /**
      * 健康检查
@@ -20,6 +24,8 @@ public class HealthCheckController {
      * @return Result.success
      */
     @GetMapping("/healthCheck")
+    @ApiOperation(value = "健康检查", notes = "不需要token")
+    @ApiVersion(group = { BaseConst.ApiVersion.V_4_0_0 })
     @Router(name = "健康检查", code = "healthCheck", level = BaseConst.PermLevel.OPEN)
     public Result healthCheck() {
         return Result.success();
